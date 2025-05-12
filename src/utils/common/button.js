c5dc0bf9cb1c3d60690c04330d9a2bfd15c54051
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-05-12 15:36:27 +0800

    style: add style for control panel icon button

--- src/utils/common/button.js
+++ src/utils/common/button.js
@@ -5,6 +5,12 @@ export const buttonBase = clsx(
   'rounded-md pr-10 pl-10 pt-2 pb-2',
 );
 
+export const controlPanelButton = clsx(
+  'w-10 h-10',
+  'flex items-center justify-center',
+  'rounded-full bg-[var(--secondary-clr)] text-[var(--base-black)]',
+)
+
 export const buttonVariants = {
   primaryOutline: 'outline-2 outline-[var(--base-white)] ',
 };
