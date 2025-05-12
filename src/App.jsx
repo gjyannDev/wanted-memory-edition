70f34c6ebde0169a59db07b9fe70dc8f41311455
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-05-12 15:04:48 +0800

    chore: add start screen and finish it

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import StartScreen from "./screens/StartScreen"
 
 export default function App() {
   return (
-    <div>
+    <div className="container">
       <StartScreen />
     </div>
   )
--- src/screens/StartScreen.jsx
+++ src/screens/StartScreen.jsx
@@ -1,9 +1,32 @@
+import clsx from "clsx";
 import backgroundVideo from "../assets/videos/bakcground_video.mp4";
 import Header from "../components/Header";
+import { buttonBase, buttonVariants } from "../utils/common/button";
+
+function StartButtonChoices() {
+  const primary_btn_outline = clsx(buttonBase, buttonVariants["primaryOutline"]);
+
+  return (
+    <div className="flex flex-col gap-[var(--size-32px)]">
+      <button
+        className={primary_btn_outline}
+        type="button"
+      >
+        Start Game
+      </button>
+      <button
+        className={primary_btn_outline}
+        type="button"
+      >
+        Match Log
+      </button>
+    </div>
+  );
+}
 
 export default function StartScreen() {
   return (
-    <div className="flex h-screen border justify-center">
+    <div className="flex h-screen justify-center">
       <video
         className="fixed top-0 left-0 w-full h-full object-cover -z-10"
         src={backgroundVideo}
@@ -12,7 +35,10 @@ export default function StartScreen() {
         muted
         playsInline
       ></video>
-    <Header />
+      <div className="flex flex-col gap-[var(--size-80px)] items-center">
+        <Header />
+        <StartButtonChoices />
+      </div>
     </div>
   );
 }
