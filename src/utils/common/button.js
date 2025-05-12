92e6d4468289c1312bf18960ec5ac527682a4bcf
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-05-12 15:04:07 +0800

    style: style for primary outline button

---
+++ src/utils/common/button.js
@@ -0,0 +1,10 @@
+import clsx from "clsx";
+
+export const buttonBase = clsx(
+  'font-bangers text-[var(--text-secondary)] text-3xl ',
+  'rounded-md pr-10 pl-10 pt-2 pb-2',
+);
+
+export const buttonVariants = {
+  primaryOutline: 'outline-2 outline-[var(--base-white)] ',
+};
