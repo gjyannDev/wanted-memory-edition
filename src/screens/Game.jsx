350f694a3841155dcc1247878885fe78d70afe8c
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-05-16 15:22:17 +0800

    chore: add game header with score board

--- src/screens/Game.jsx
+++ src/screens/Game.jsx
@@ -1,18 +1,22 @@
+import clsx from "clsx";
 import { useState } from "react";
 import onePieceLogo from "../assets/images/one_piece_logo.png";
+import { scoreBoardText } from "../utils/common/utils";
 
 function GameHeader({ currentScore, bestScore }) {
+  const score_text_style = clsx(scoreBoardText);
+
   return (
-    <div className="">
-      <img src={onePieceLogo} alt="one piece png logo" />
-      <div className="">
-        <div className="">
-          <p>Score: </p>
-          <p>{currentScore}</p>
+    <div className="flex w-screen justify-between p-5 ">
+      <img className="w-[12rem]" src={onePieceLogo} alt="one piece png logo" />
+      <div className="flex flex-col gap-[var(--size-4px)]">
+        <div className="flex gap-2">
+          <p className={score_text_style}>Score: </p>
+          <p className={score_text_style}>{currentScore}</p>
         </div>
-        <div className="">
-          <p>Best Score: </p>
-          <p>{bestScore}</p>
+        <div className="flex gap-2">
+          <p className={score_text_style}>Best Score: </p>
+          <p className={score_text_style}>{bestScore}</p>
         </div>
       </div>
     </div>
