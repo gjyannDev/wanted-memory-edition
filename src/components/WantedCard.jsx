a212114e078a8d298a6dd805613da5b781e491be
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-05-16 23:22:59 +0800

    style: adjust the card width and height and also adjus the others style

--- src/components/WantedCard.jsx
+++ src/components/WantedCard.jsx
@@ -3,22 +3,22 @@ import luffyImage from ".././assets/images/luffy_image.jpg";
 export default function WantedCard() {
   return (
     <div className="flex justify-center">
-      <div className="w-[14.25rem] h-[21.25rem] bg-[var(--card-bg)] flex flex-col items-center gap-3.5 pt-2.5 pb-2.5">
-        <div className="flex flex-col gap-2">
-          <p className="text-4xl text-center font-merriweather-bold text-[var(--wanted-text-clr)]">
+      <div className="w-[11.25rem] h-[16.25rem] bg-[var(--card-bg)] flex flex-col items-center gap-3 pt-2 pb-2">
+        <div className="flex flex-col gap-1.5">
+          <p className="text-2xl text-center font-merriweather-bold text-[var(--wanted-text-clr)]">
             WANTED
           </p>
           <img
-            className="w-[188px] h-[156px] border border-[var(--outline-img)]"
+            className="w-[140px] h-[110px] border border-[var(--outline-img)] object-cover"
             src={luffyImage}
             alt="luffy image jpeg"
           />
         </div>
-        <div className="flex flex-col gap-3.5">
-          <p className="text-2xl text-center font-merriweather-medium text-[var(--wanted-text-clr)]">
+        <div className="flex flex-col gap-2.5">
+          <p className="text-base text-center font-merriweather-medium text-[var(--wanted-text-clr)]">
             DEAD OR ALIVE
           </p>
-          <p className="text-2xl text-center tracking-tighter font-merriweather-bold text-[var(--wanted-text-clr)]">
+          <p className="text-lg text-center tracking-tight font-merriweather-bold text-[var(--wanted-text-clr)]">
             MONKEY D LUFFY
           </p>
         </div>
