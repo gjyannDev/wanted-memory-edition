db7b0752ec0a0d5b326b09cec0c2ab8048ec0a85
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-05-16 09:33:46 +0800

    chore: added game start screen and implement button click to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import StartScreen from "./screens/StartScreen"
+import { useState } from "react";
+import GameStartScreen from "./screens/GameStartScreen";
+import StartScreen from "./screens/StartScreen";
 
 export default function App() {
+  const [isGameStarted, setIsGameStarted] = useState(false);
+
   return (
     <div className="">
-      <StartScreen />
+      {isGameStarted ? (
+        <GameStartScreen />
+      ) : (
+        <StartScreen setIsGameStarted={setIsGameStarted} />
+      )}
     </div>
-  )
+  );
 }
