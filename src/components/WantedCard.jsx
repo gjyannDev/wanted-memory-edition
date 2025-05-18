f6de64a3b6f751939ca8ca8a1bb75f2fc039e436
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-05-18 19:59:06 +0800

    chore: created the logic for the game, adding more mode later and implement the custom hook for the game logic

--- src/components/WantedCard.jsx
+++ src/components/WantedCard.jsx
@@ -1,8 +1,8 @@
 export default function WantedCard({ characterData }) {
   return (
     <div className="flex justify-center">
-      {characterData.map((data) => (
-        <div className="w-[11.25rem] h-[16.25rem] bg-[var(--card-bg)] flex flex-col items-center gap-3 pt-2 pb-2">
+      {characterData.map((data, index) => (
+        <div key={index} className="w-[11.25rem] h-[16.25rem] bg-[var(--card-bg)] flex flex-col items-center gap-3 pt-2 pb-2">
           <div className="flex flex-col gap-1.5">
             <p className="text-2xl text-center font-merriweather-bold text-[var(--wanted-text-clr)]">
               WANTED
--- src/screens/Game.jsx
+++ src/screens/Game.jsx
@@ -1,10 +1,15 @@
 import clsx from "clsx";
-import { useState } from "react";
+import _ from "lodash";
+import { useEffect, useMemo, useState } from "react";
 import onePieceLogo from "../assets/images/one_piece_logo.png";
 import WantedCard from "../components/WantedCard";
 import FetchData from "../services/FetchData";
 import { scoreBoardText } from "../utils/common/utils";
-import { filteredCharacters } from "../utils/utility";
+import {
+  filteredCharacters,
+  getRandomIntBasedOnDifficulty,
+  selected_characters,
+} from "../utils/utility";
 
 function GameHeader({ currentScore, bestScore }) {
   const score_text_style = clsx(scoreBoardText);
@@ -26,35 +31,29 @@ function GameHeader({ currentScore, bestScore }) {
   );
 }
 
+//TODO: Separate the logic of the game to a new cutstom hook to make it more resuable and manageable
 export default function Game({ mode }) {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
   const { fetchCharacterDetails } = FetchData();
-  const selected_characters = [
-    "Monkey D., Luffy",
-    "Sanji",
-    "Jinbe",
-    "Brook",
-    "Usopp",
-    "Nami",
-    "Roronoa, Zoro",
-    "Franky",
-    "Vegapunk",
-    "Nico, Robin",
-    "Tony Tony, Chopper",
-    "Shanks",
-    "Shirahoshi",
-    "Nefertari Vivi",
-    "Portgas D,. Ace",
-    "Sabo",
-    "Boa Hancock",
-  ];
-  const filterd_data = filteredCharacters(fetchCharacterDetails, selected_characters);
+  const [filteredData, setFilteredData] = useState([]);
+  const character_data = useMemo(
+    () => filteredCharacters(fetchCharacterDetails, selected_characters),
+    [fetchCharacterDetails]
+  );
+
+  useEffect(() => {
+    const shuffled_data = _.shuffle(character_data);
+
+    if (mode === "Easy") {
+      setFilteredData(shuffled_data.slice(0, 3));
+    }
+  }, [character_data, mode]);
 
   return (
     <div className="">
       <GameHeader currentScore={score} bestScore={bestScore} />
-      <WantedCard characterData={filterd_data}/>
+      <WantedCard characterData={filteredData} />
     </div>
   );
 }
