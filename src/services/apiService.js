85503e33e02821bc4805fd6517082e7952d40c79
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-05-23 00:26:51 +0800

    chore: add api for adding the player data to the firestore database

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,9 +1,26 @@
+import { arrayUnion, updateDoc } from "firebase/firestore";
+import { user_ref } from "./firebaseClient";
+
 export async function getAnimeCharacterDetails() {
   try {
     const res = await fetch(`https://api.jikan.moe/v4/anime/${21}/characters?`);
     const data = res.json();
 
     return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function addPlayerData(playerData) {
+  try {
+    const res = await updateDoc(
+      user_ref,
+      { playerData: arrayUnion(playerData) },
+      { merge: true }
+    );
+
+    return res;
   } catch (error) {
     console.error(error);
   }
