bff678ee0c4eed170d4682f25a016884a06dcccc
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-05-23 08:54:37 +0800

    chore: add fetching for all players data and also implemented loading functionality when the data is stil being fetched

--- src/services/FetchData.jsx
+++ src/services/FetchData.jsx
@@ -1,16 +1,23 @@
 import { useEffect, useState } from "react";
-import { getAnimeCharacterDetails } from "./apiService";
+import { getAnimeCharacterDetails, getPlayerData } from "./apiService";
 
 export default function FetchData() {
   const [fetchCharacterDetails, setFetchCharacterDetails] = useState({});
+  const [fetchAllPlayersData, setFetchAllPlayersData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
       try {
         const get_character_details = await getAnimeCharacterDetails();
         setFetchCharacterDetails(get_character_details.data || {});
+
+        const get_all_players_data = await getPlayerData();
+        setFetchAllPlayersData(get_all_players_data || {});
       } catch (error) {
         console.error("Error fetching data: ", error);
+      } finally {
+        setIsLoading(false);
       }
     }
     fetchData();
@@ -19,5 +26,8 @@ export default function FetchData() {
   return {
     fetchCharacterDetails,
     setFetchCharacterDetails,
+    fetchAllPlayersData,
+    setFetchAllPlayersData,
+    isLoading,
   };
 }
